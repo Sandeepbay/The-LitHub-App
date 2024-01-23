87a8d6b97f65f752c792d44011aaede45bc0b74b
sandeepbay2002@gmail.com <sandeepbay2002@gmail.com>
2024-01-23 20:43:58 +0530

    Completed CRUD Operations

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const methodOveride = require('method-override')
 const app = express()
 const path = require('path')
 const Book = require('./models/book')
@@ -15,6 +16,9 @@ db.once('open' , () => {
 app.set('view engine' , 'ejs')
 app.set('views' , path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true}))
+app.use(methodOveride('_method'))
+
 app.get('/' , (req,res) => {
     res.render('home')
 })
@@ -24,11 +28,38 @@ app.get('/books', async (req,res) => {
     res.render('books/index' , {books})
 })
 
+app.get('/books/new' , (req,res) => {
+    res.render('books/new')
+})
+
+app.post('/books' , async(req,res) => {
+    const book = new Book(req.body.book)
+    await book.save()
+    res.redirect(`/books/${book._id}`)
+})
+
 app.get('/books/:id' , async (req,res) => {
     const book = await Book.findById(req.params.id)
     res.render('books/show' , {book})
 })
 
-app.listen(3000 , (re,res) => {
+app.get('/books/:id/edit' , async(req,res) => {
+    const book = await Book.findById(req.params.id)
+    res.render('books/edit' , {book})
+})
+
+app.put('/books/:id' , async(req,res) => {
+    const { id } = req.params
+    const book = await Book.findByIdAndUpdate(id , {...req.body.book})
+    res.redirect(`/books/${book._id}`)
+})
+
+app.delete('/books/:id' , async(req,res) => {
+    const {id} = req.params
+    await Book.findByIdAndDelete(id)
+    res.redirect('/books')
+})
+
+app.listen(3000 , () => {
     console.log("Listening at Port 3000")
 })
