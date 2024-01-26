8f43753890ca89c9f70ee44d094ccd943c53a694
sandeepbay2002@gmail.com <sandeepbay2002@gmail.com>
2024-01-27 00:23:08 +0530

    Handling More Errors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const methodOveride = require('method-override')
 const ejsMate = require('ejs-mate')
+const catchAsync = require('./Utility/catchAsync')
 const app = express()
 const path = require('path')
 const Book = require('./models/book')
@@ -26,47 +27,42 @@ app.get('/' , (req,res) => {
     res.render('home')
 })
 
-app.get('/books', async (req,res) => {
+app.get('/books', catchAsync(async (req,res) => {
     const books = await Book.find({})
     res.render('books/index' , {books})
-})
+}))
 
 app.get('/books/new' , (req,res) => {
     res.render('books/new')
 })
 
-app.post('/books' , async (req,res,next) => {
-    try {        
-        const book = new Book(req.body.book)
-        await book.save()
-        res.redirect(`/books/${book._id}`)
-    } catch(e) {
-        next(e)
-    }
-    
-})
+app.post('/books' , catchAsync(async (req,res,next) => {     
+    const book = new Book(req.body.book)
+    await book.save()
+    res.redirect(`/books/${book._id}`)
+}))
 
-app.get('/books/:id' , async (req,res) => {
+app.get('/books/:id' , catchAsync(async (req,res) => {
     const book = await Book.findById(req.params.id)
     res.render('books/show' , {book})
-})
+}))
 
-app.get('/books/:id/edit' , async(req,res) => {
+app.get('/books/:id/edit' , catchAsync(async(req,res) => {
     const book = await Book.findById(req.params.id)
     res.render('books/edit' , {book})
-})
+}))
 
-app.put('/books/:id' , async(req,res) => {
+app.put('/books/:id' , catchAsync(async(req,res) => {
     const { id } = req.params
     const book = await Book.findByIdAndUpdate(id , {...req.body.book})
     res.redirect(`/books/${book._id}`)
-})
+}))
 
-app.delete('/books/:id' , async(req,res) => {
+app.delete('/books/:id' , catchAsync(async(req,res) => {
     const {id} = req.params
     await Book.findByIdAndDelete(id)
     res.redirect('/books')
-})
+}))
 
 app.use((err,req,res,next) => {
     res.send("Something went wrong")
