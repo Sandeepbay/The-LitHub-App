2d28d1d1c83b2f2bd6002bf1e137ff13e088853c
sandeepbay2002@gmail.com <sandeepbay2002@gmail.com>
2024-02-27 00:53:01 +0530

    Making few Teaks

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,26 @@ const seedDB = async () => {
             title: book.title,
             author: book.author,
             owner: '65d8b250749cfa198c19730a',
-            image: `https://loremflickr.com/300/300/books?random=5`,
             description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate vel necessitatibus sed sint quibusdam, ad numquam laborum dolorem quia provident aliquam id modi dolores rem! Natus hic accusantium quo! Animi.Excepturi facere numquam ab tenetur. Officiis sit, magni iusto vero temporibus neque quam voluptates nesciunt, eius vel odio recusandae mollitia unde. Minus ratione officiis quas nostrum laudantium sunt, cumque nulla.',
             price,
+            images: [
+                {
+                  url: 'https://res.cloudinary.com/du7dfoknu/image/upload/v1708972762/LitHub/w8e8bxsnivlsmwduwuwi.jpg',
+                  filename: 'LitHub/w8e8bxsnivlsmwduwuwi',
+                },
+                {
+                  url: 'https://res.cloudinary.com/du7dfoknu/image/upload/v1708972765/LitHub/jzrtwfbx57cjwbo3jhli.jpg',
+                  filename: 'LitHub/jzrtwfbx57cjwbo3jhli',
+                },
+                {
+                  url: 'https://res.cloudinary.com/du7dfoknu/image/upload/v1708972770/LitHub/zjeobvf52frsdny2qjjk.jpg',
+                  filename: 'LitHub/zjeobvf52frsdny2qjjk',
+                },
+                {
+                  url: 'https://res.cloudinary.com/du7dfoknu/image/upload/v1708972771/LitHub/jmsu6dbxj4dvc75obvf4.jpg',
+                  filename: 'LitHub/jmsu6dbxj4dvc75obvf4',
+                }
+            ],
         })
         await newBook.save()
     }
