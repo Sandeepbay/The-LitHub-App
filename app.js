5df420919116dae9ce482c055e7a31b47815b625
sandeepbay2002@gmail.com <sandeepbay2002@gmail.com>
2024-03-04 02:27:32 +0530

    Added Mongo Session

--- app.js
+++ app.js
@@ -23,8 +23,11 @@ const strategyLocal = require('passport-local')
 const User = require('./models/user')
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet')
+const MongoDBStore = require('connect-mongo')(session)
 
-mongoose.connect('mongodb://localhost:27017/the-lithub')
+
+const dbUrl = 'mongodb://localhost:27017/the-lithub'
+mongoose.connect(dbUrl)
 
 const db = mongoose.connection
 db.on('error' , console.error.bind(console , 'Connection error:'))
@@ -41,7 +44,14 @@ app.use(express.urlencoded({ extended: true}))
 app.use(methodOveride('_method'))
 app.use(express.static(path.join(__dirname , 'public')))
 
+const store = new MongoDBStore({
+    url: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    secret: 'thishsouldbeagoodsecret'
+});
+
 const sessionConfig = {
+    store,
     name: 'session',
     secret: 'thishsouldbeagoodsecret',
     resave: false,
